e990a341311788b607ad8fe3bcdc49d43c6744f0
Maheshwaran <psmaheshwa@gmail.com>
2019-07-25 17:43:29 +0530

    last commit

--- src/GUIForm.java
+++ src/GUIForm.java
@@ -8,15 +8,15 @@ public class GUIForm {
     private JPanel panel;
     private JButton button1;
     private JTable table1;
-
-    public GUIForm() {
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                JOptionPane.showMessageDialog(null,"Hello World");
-            }
-        });
-    }
+//
+//    public GUIForm() {
+//        button1.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent actionEvent) {
+//                JOptionPane.showMessageDialog(null,"Hello World");
+//            }
+//        });
+//    }
 
     public static void main(String[] args) throws UnknownHostException {
         JFrame frame = new JFrame("IP Scanner");
