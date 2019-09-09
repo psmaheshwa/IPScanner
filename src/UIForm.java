654d146f3e0b57a912c541c187aed2234d182728
mattew <psmaheshwa@gmail.com>
2019-09-09 16:17:57 +0530

    netcat

--- src/UIForm.java
+++ src/UIForm.java
@@ -19,6 +19,10 @@ public class UIForm extends JFrame{
     private JTable table1;
     private JButton shutdownButton;
     private JTextField shutdownip;
+    private JLabel deviceiplabel;
+    private JLabel tolabel;
+    private JLabel enteriplabel;
+    private JLabel iprangelable;
     private boolean notifyloop = false;
     private boolean scanloop = false;
     private Thread runner = null;
